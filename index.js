35f45781487a98a44d0fc9316efb41fcf6bf7983
Marcos Gutierrez <gmarcos87@gmail.com>
2019-12-09 15:28:39 -0300

    start express server

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+const Server = express()
+const log = require('fancy-log')
+const logSymbols = require('log-symbols')
+const app = require('./lib/app')
+
+const port = process.env.PORT || 3000
+
+Server.use(app)
+
+Server.listen(port,() => {
+    log.info(`${logSymbols.info} Server starterd at port ${port}`)
+})
