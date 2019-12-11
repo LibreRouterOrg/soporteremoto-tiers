9a1ed9eef891b464de54201c71a9007a6a1af0fd
Marcos Gutierrez <gmarcos87@gmail.com>
2019-12-11 15:02:24 -0300

    add tests

---
+++ tests/scripts/genKey.test.js
@@ -0,0 +1,37 @@
+const db = require('../../src/db')
+const { generateRandomKey, handlePut} = require('../../scripts/genKey')
+
+beforeAll(done => {
+    db.clear(done)
+})
+
+afterAll(done => {
+    db.close(done)
+});
+
+describe('Key generation script', ()=> {
+    let keyGenerated
+    
+    it('should generate a random and unique key in the db', (done) => {
+        generateRandomKey(key => {
+            keyGenerated = key
+            expect(key).toHaveLength(12)
+            done()
+        })
+    })
+
+    it('should find the generated key in the database', (done) => {
+        db.get(keyGenerated, (error, value) => {
+            expect(error).toBeNull()
+            expect(value).toEqual('{}')
+            done()
+        })
+    })
+
+    it('should return an error if something fails', (done) => {
+        handlePut('testkey',(data)=>{
+            expect(data).toEqual(new Error('Error when saving to db'))
+            done()
+        })(new Error('Error when saving to db'))
+    })
+})
