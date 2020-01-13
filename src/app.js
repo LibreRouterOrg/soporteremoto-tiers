200aa8fff0c9b638a3008c0b5e4cb36f1f75aa12
Marcos Gutierrez <gmarcos87@gmail.com>
2020-01-13 16:28:53 -0300

    send ssh public key and save support request

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const logSymbols = require('log-symbols')
 let log = require('fancy-log')
 const db = require('./db')
 const addToLibreNet6 = require('./addToLibreNet6')
+const generateRSAKeypair = require('generate-rsa-keypair')
+const forge = require('node-forge')
 
 if(process.env.NODE_ENV === 'test') {
     log = console
@@ -57,6 +59,53 @@ app.post('/submit', (req, res)=> {
     })
 })
 
+app.post('/support-request', async (req, res) => {
+    if (!req.body.apiKey) { 
+        res.json({ error: 'missing api key' })
+        return
+    };
+
+    db.get(req.body.apiKey, (err, value) => {
+        if(err) {
+            //wait 2 seconds if the api key is invalid
+            setTimeout(()=> res.json({ error: 'wrong api key' }), 2000)
+            log.error(logSymbols.error, 'Wrong api key used: '+ req.body.apiKey)
+            return
+        }
+        try {
+            value = JSON.parse(value)
+        } catch(e) { 
+            log.warn(logSymbols.warning, 'Error getting the information from '+ req.body.apiKey)
+            res.json({ error: 'The configuration could not be saved' })
+            return;
+        }
+
+        if (value.network) {
+            try {
+                //Generate ssh key pair
+                const keys = generateRSAKeypair()
+                let toSend = forge.pki.publicKeyFromPem(keys.public)
+                toSend = forge.ssh.publicKeyToOpenSSH(toSend).split(' ')[1]
+
+                //Attach to previous keys
+                value.keys = value.keys? [...value.keys, { keys }]: [{ keys }]
+                value.requests = value.requests? [...value.requests, req.body.request] : [ req.body.request]
+
+                //Save and send response
+                db.put(req.body.apiKey, JSON.stringify(value), (err) => {
+                    if (err) { return res.json({ error: 'The configuration could not be saved' }) }
+                    return res.json({ sshKey: toSend, name: 'remote-support' })
+                })
+            } catch (e) {
+                console.log('Error on support request', e)
+                return res.json({e})
+            }
+        } else {
+            return res.json({ error: 'The configuration could not be saved' })
+        }
+
+    })
+})
 module.exports = {
     app,
     handlePut
